a557df299891259320522539f028d6d9d5e72676
dyleenwilard <dyleenwilard@gmail.com>
2016-07-23 21:17:20 +0000

    Completion of info responses

--- app/Http/Controllers/Sms/IncomingController.php
+++ app/Http/Controllers/Sms/IncomingController.php
@@ -22,6 +22,7 @@ class IncomingController extends Controller
     {
         file_put_contents(base_path('logs/sms/inbound/').'inbound_'.date('Ymd').'.log', '['.date('Y-m-d H:i:s').']'. json_encode($request->all()) . "\n" , FILE_APPEND);
         $explodedMessage = explode(' ', $request['message']);
+        $referenceID = strtoupper("DESC". substr(md5(uniqid(rand(), true)), 10, 17)) . time();
         $validateCluster = Cluster::where('cluster_code', strtoupper($explodedMessage[0]))->first();
         if(empty($validateCluster))
         {
@@ -34,7 +35,7 @@ class IncomingController extends Controller
             $responseMessage = str_replace('{#cluster}', "\n".$substitute, $invalidMessage['message']);
             $dataQueue = [
                 'cluster_id' => 0,
-                'reference_id' => strtoupper("DESC" . substr(md5(uniqid(rand(), true)), 10, 17)) . time(),
+                'reference_id' => $referenceID,
                 'number'=> $request['mobile_number'],
                 'sms_type' => 1,
                 'message' => $responseMessage,
@@ -42,37 +43,51 @@ class IncomingController extends Controller
                 'request_id' => $request['request_id']
             ];
         } else {
-            $referenceID = strtoupper("DESC". substr(md5(uniqid(rand(), true)), 10, 17)) . time();
-            $validMessage = CannedResponse::where('response_type', 1)->first();
-            $responseMessage = str_replace('{#cluster}', $validateCluster->name, $validMessage['message']);
-            $dataQueue = [
-                'cluster_id' => $validateCluster->id,
-                'reference_id' => $referenceID,
-                'number'=> $request['mobile_number'],
-                'sms_type' => 1,
-                'message' => $responseMessage,
-                'time_prepared' => date('Y-m-d H:i:s'),
-                'request_id' => $request['request_id']
-            ];
-            unset($explodedMessage[0]);
-            $inboundMessage = implode(" ", $explodedMessage);
+            $infoResponse = CannedResponse::where('info_code', strtoupper($explodedMessage[1]))->where('cluster_id', $validateCluster->id)->where('response_type', 3)->first();
+            if($infoResponse){
+                $validMessage = CannedResponse::where('response_type', 1)->first();
+                $responseMessage = str_replace('{#cluster}', $validateCluster->name, $validMessage['message']);
+                $dataQueue = [
+                    'cluster_id' => $validateCluster->id,
+                    'reference_id' => $referenceID,
+                    'number'=> $request['mobile_number'],
+                    'sms_type' => 1,
+                    'message' => $infoResponse->message,
+                    'time_prepared' => date('Y-m-d H:i:s'),
+                    'request_id' => $request['request_id']
+                ];
+            } else { 
+                $validMessage = CannedResponse::where('response_type', 1)->first();
+                $responseMessage = str_replace('{#cluster}', $validateCluster->name, $validMessage['message']);
+                $dataQueue = [
+                    'cluster_id' => $validateCluster->id,
+                    'reference_id' => $referenceID,
+                    'number'=> $request['mobile_number'],
+                    'sms_type' => 1,
+                    'message' => $responseMessage,
+                    'time_prepared' => date('Y-m-d H:i:s'),
+                    'request_id' => $request['request_id']
+                ];
+                unset($explodedMessage[0]);
+                $inboundMessage = implode(" ", $explodedMessage);
+                
+                $data = [
+                    'cluster_id' => $validateCluster->id,
+                    'reference_id' => $referenceID,
+                    'number' => $request['mobile_number'],
+                    'message' => $inboundMessage,
+                    'time_received' => date('Y-m-d H:i:s', $request['timestamp']),
+                    'request_id' => $request['request_id']
+                ];    
+                
+                Inbound::insert($data);
+                Curl::to(url("api/modules/ticket"))
+                ->withData(["reference_id" => $data['reference_id'], "user_id" => 1, "cluster_id" => $validateCluster->id, "ticket_status" => 0])
+                ->post();
             
-            $data = [
-                'cluster_id' => $validateCluster->id,
-                'reference_id' => $referenceID,
-                'number' => $request['mobile_number'],
-                'message' => $inboundMessage,
-                'time_received' => date('Y-m-d H:i:s', $request['timestamp']),
-                'request_id' => $request['request_id']
-            ];    
-            
-            Inbound::insert($data);
-            Curl::to(url("api/modules/ticket"))
-            ->withData(["reference_id" => $data['reference_id'], "user_id" => 1, "cluster_id" => $validateCluster->id, "ticket_status" => 0])
-            ->post();
-        
-        $redis = Redis::connection();
-        $redis->publish('notification.' . $validateCluster->id, $referenceID);            
+                $redis = Redis::connection();
+                $redis->publish('notification.' . $validateCluster->id, $referenceID);                            
+            }
         }
         
         Queue::queue($dataQueue);
